d9d58b0b05a1468f35826ee1d4338ae1ebfa791f
Patrick Kalkman <patrick@simpletechture.nl>
2020-01-31 23:30:56 +0100

    Logging secrets reading

--- WorkflowEngine/lib/config/secrets.js
+++ WorkflowEngine/lib/config/secrets.js
@@ -8,12 +8,7 @@ dockerSecret.read = function read(secretName) {
   try {
     return fs.readFileSync(`/run/secret/${secretName}`, 'utf8');
   } catch(err) {
-    if (err.code !== 'ENOENT') {
-      log.error(`An error occurred while trying to read the secret: ${secretName}. Err: ${err}`);
-    } else {
-      log.debug(`Could not find the secret, probably not running in swarm mode: ${secretName}. Err: ${err}`);
-    }
-    
+    log.error(`An error occurred while trying to read the secret: ${secretName}. Err: ${err}`);
     return false;
   }
 };
